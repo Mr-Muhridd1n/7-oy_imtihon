3dba757f53675c505cb406b18d0e2105a9b97517
Muhriddin <msayfuddinov7@gmail.com>
2025-08-13 14:35:58 +0500

    push

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const BASE_URL = "https://json-api.uz/api/project/dessertss/desserts";
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
